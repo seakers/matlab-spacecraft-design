bc678a3484a09248b0c6e37846dd7a318e9e3306
nen33 <nen33@cornell.edu>
2016-08-04 14:10:59 -0400

    Delete order.m

--- order.m
+++
@@ -1,8 +0,0 @@
-function n = order(val, base)
-%function taken from Ivar Eskerud Smith on
-%mathworks.com/matlabcentral/fileexchange/28559-order-of-magnitude/content/order.m
-
-if nargin < 2
-    base = 10;
-end
-n = floor(log(abs(val))./log(base));
