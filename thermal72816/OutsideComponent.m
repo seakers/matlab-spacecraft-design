71a4e96980fbd208209c330cc528f2af76a3e32c
jf442 <jf442@cornell.edu>
2016-08-04 13:22:27 -0400

    Add files via upload

---
+++ thermal72816/OutsideComponent.m
@@ -0,0 +1,75 @@
+function complist=OutsideCompoent(complist,MLI,outpaint)
+len=length(complist);
+epsilon=outpaint.emittance;
+sigma=5.67e-8;
+
+for i=1:len
+    
+    if strcmp('Outside',complist(i).LocationReq)
+        
+        Qw=complist(i).Power;
+        
+        dim=complist(i).Dim;
+        
+        if strcmp('Rectangle',complist(i).Shape)
+            area=2*(dim(1)*dim(2)+dim(1)*dim(3)+dim(2)*dim(3));
+        elseif strcmp('Cylinder',complist(i).Shape)
+            area=0.5*pi*dim(1)^2+pi*dim(1)*dim(2);
+        elseif strcmp('Sphere',complist(i).Shape)
+            area=pi*dim(1)^2;
+        end
+        
+        %Qcondheater=MLI.conductivity*area*(complist(i).Thermal(1)+273-x);
+        %Qcondcooler=MLI*conductivity*area*(complist(i).Thermal(2)+273-x);
+        %Qrad=0.3*sigma*epsilon*area*x^4;
+       
+        %find the x as the outpaint temperature, and thus find the heat out
+        %from the conduction and insulation balance: if the heat out is
+        %greater than heatpower of the component, that means the component
+        %needs heater and the heater power needed is the difference between
+        %conduction heat output of (T operation low- x) and heatpower of
+        %the component. If conduction heat out is smaller than heatpower
+        
+       %same method if the conduction heat out is smaller than the
+       %heatpower of the component, that means heat pipe is needed to take
+       %the heat that the component cannot dissipate out to the radiator of
+       %the satellite and the power needed of the heat pipe is the
+       %difference between heatpower and conduction heat
+        
+        hotfun=@(x,a,b,c,d,e,f)a*b*(c-x)/d-0.3*e*f*b*x^4;
+        a=MLI.conductivity;
+        b=area;
+        c=complist(i).Thermal(1)+273;
+        d=MLI.thickness;
+        e=sigma;
+        f=epsilon;
+        fun=@(x)hotfun(x,a,b,c,d,e,f);
+        T=fzero(fun,1000);
+        Qout=0.3*epsilon*sigma*area*T^4;
+        if Qout>Qw
+        complist(i).HeaterPower=Qout-Qw;
+        end
+        
+        coldfun=@(x,a,b,c,d,e,f)a*b*(c-x)/d-0.3*e*f*b*x^4;
+        a=MLI.conductivity;
+        b=area;
+        c=complist(i).Thermal(2)+273;
+        d=MLI.thickness;
+        e=sigma;
+        f=epsilon;
+        fun=@(x)coldfun(x,a,b,c,d,e,f);
+        T=fzero(fun,1000);
+        Qout=0.3*epsilon*sigma*area*T^4;
+        if Qout<Qw
+            complist(i).HeatPipePower=Qw-Qout;
+        end
+        
+        %used a factor of 0.3 to account for the backload as well as the
+        %solar input in the space environment
+        %confirm with P.Selva if a cooler environment is needed.
+        
+        
+        
+        
+    end
+end
---
+++ thermal72816/thermal728.m
@@ -0,0 +1,60 @@
+function [thermal,complist]=thermal726(x,y,z,H,complist)
+%for the purpose of merging code into the main code on 7/14/16 meeting
+%for faster result
+Thigh=35;
+Tlow=5;
+
+powertotal=sum(cat(1,complist.HeatPower));
+
+%load the Comps list here as needed
+%load the radiator location as needed.
+Rlocation.x=0.375;
+Rlocation.y=0.375;
+Rlocation.z=0;
+
+
+%infofunc should include xyz of the spacecraft, altitude and total power
+infofunc.H=H;
+infofunc.Thigh=Thigh;
+infofunc.Tlow=Tlow;
+infofunc.x=x;
+infofunc.y=y;
+infofunc.z=z;
+infofunc.A=2*(x*y+x*z+y*z);
+infofunc.pmax=powertotal;
+infofunc.pmin=powertotal;
+[Radiator]=ThermalEstimate(infofunc);
+%there's radiator type, radiator area, radiator dimension, radiator louver
+%angle, and radiator mass
+
+%inside component's heatpipe, cooler and heater
+MLI=MLIproperties;
+inpaint=inpaintProperties;
+outpaint=outpaintProperties;
+complist=OutsideComponent(complist,MLI,outpaint);
+[complist,thermal]=InsideComponent(complist,Tlow,Thigh,MLI,inpaint);
+
+
+
+Heatpipe=HeatPipeProperties;
+CHP=HeatPipeonComponents(complist,Rlocation,Heatpipe);
+%radiator=Rplacement(R_conf,infofunc);
+
+[complist,MLImass,MLIcost]=preMLI(complist,MLI,Thigh,Tlow,infofunc);
+outpaint=outcoating(complist,infofunc,Radiator);
+inpaint=incoating(complist);
+
+
+thermal.mass=0.0607*9.5; %drymass assume to be 9.5
+
+
+thermal.mass_cjf=outpaint.mass+inpaint.mass+MLImass+CHP.mass;
+thermal.mass_wradiator=thermal.mass_cjf+Radiator.mass;
+thermal.outpaintmass=outpaint.mass;
+thermal.inpaintmass=inpaint.mass;
+thermal.mlimass=MLImass;
+thermal.radiatormass=Radiator.mass;
+thermal.chpmass=CHP.mass;
+thermal.cost=394*thermal.mass^(.635)+50.6*thermal.mass^.707;
+%thermal.cost_cjf=outpaint.cost+inpaint.cost+CHP.cost+RHP.cost+MLIcost;
+end
