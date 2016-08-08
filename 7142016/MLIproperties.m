359eca8be141da7a8d9f7429ef39adc25d1d053c
jf442 <jf442@cornell.edu>
2016-08-08 16:19:17 -0400

    Delete MLIproperties.m

--- 7142016/MLIproperties.m
+++
@@ -1,9 +0,0 @@
-function MLI=MLIproperties
-%all the properties are taken and calculated from Multilayer Insulation
-%Material Guidelines published by NASA in 1998. Those properties can be
-%modified as seen fit
-
-MLI.rho=0.12; 
-MLI.cost=50;  
-MLI.thickness=0.005; 
-end
